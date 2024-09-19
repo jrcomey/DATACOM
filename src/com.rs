e79b243f0a258c408db9bcc9db3b9a8dd3bc38e6
Jack <jrcomey@github.com>
2024-09-18 17:59:44 -0700

    Added packet reception protocol for recieving files in chunks

--- src/com.rs
+++ src/com.rs
@@ -1,11 +1,12 @@
-use std::io::Read;
+use std::fmt::Error;
+use std::io::{Read, Write};
 // use tokio;
 // use tokio::time::sleep;
 use std::net::{SocketAddr, TcpStream, TcpListener};
-use std::error::Error;
+// use std::error::Error;
 use std::sync::{RwLock, Arc};
 use std::time::Duration;
-
+use std::fs::File;
 use glium::debug;
 
 use crate::scenes_and_entities::Scene;
@@ -14,22 +15,78 @@ use crate::scenes_and_entities::Scene;
 
 pub fn from_network(mut stream: &TcpStream) -> String{
     // debug!("Handle Commands called");
-    let mut buffer = [0; 17000];
+    let mut buffer = [0; 600000];
     let bytes_read = stream.read(&mut buffer).unwrap();
     let packet = String::from_utf8_lossy(&buffer[..bytes_read]);
     // println!("{}",packet);
     return packet.to_string();
 }
 
-pub fn run_server(scene_reference: Arc<RwLock<Scene>>, addr: std::net::SocketAddr) {
-    let listener = TcpListener::bind(addr).unwrap();
-        match listener.accept() {
-            Ok((stream, _)) => {
-                loop{
-                    let packet = from_network(&stream);
-                    scene_reference.write().unwrap().cmd_msg_str(packet.as_str());
-                }
+// Rewrite me to be better
+pub fn from_network_with_protocol(stream: &mut TcpStream) -> Result<(), &str> {
+    // Get file name first, return an error if there are no more files
+    const CHUNK_SIZE: usize = 4096;
+    let mut name_buffer = [0; 400];
+    let name_bytes_read = stream.read(&mut name_buffer).unwrap();
+    let name = String::from_utf8_lossy(&name_buffer[..name_bytes_read]).to_string();
+    debug!("Name: {}", name);
+    if name == "END" {
+        return Err("Done");
+    }
+    else {
+        // Open the file
+        let mut file = File::create(&name).unwrap();
+        
+        // Get file size next
+        let mut file_size_buffer = [0; 8];
+        stream.read_exact(&mut file_size_buffer);
+        let mut bytes_received = 0;
+        let file_size = u64::from_be_bytes(file_size_buffer);
+        debug!("Anticipated file size: {} bytes", file_size);
+
+        
+
+        let mut buffer = [0; CHUNK_SIZE];
+        let mut num_packets_recieved = 0;
+        while bytes_received < file_size {
+            let bytes_read = stream.read(&mut buffer).unwrap();
+            if bytes_read == 0 {
+                break;
             }
-            _ => {;},
+            file.write_all(&buffer[..bytes_read]).unwrap();
+            bytes_received += bytes_read as u64;
+            debug!("Received packet #{}", num_packets_recieved);
+            num_packets_recieved += 1;
         }
+        
+        // Recieve file in chunks
+        
+
+    }
+    Ok(())
+}
+
+pub fn run_server(scene_reference: Arc<RwLock<Scene>>, addr: std::net::SocketAddr) {
+    info!("Server started!");
+    let listener = TcpListener::bind(addr).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let packet = from_network(&stream);
+                debug!("Packet: {}", packet.as_str());
+                scene_reference.write().unwrap().cmd_msg_str(&packet.as_str());
+            },
+            Err(_) => {;}
+        }
+    }
+        // match listener.accept() {
+        //     Ok((stream, _)) => {
+        //         loop{
+        //             let packet = from_network(&stream);
+        //             scene_reference.write().unwrap().cmd_msg_str(packet.as_str());
+        //         }
+        //     }
+        //     _ => {;},
+        // }
 }
